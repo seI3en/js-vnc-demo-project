f003d7a355658d79230baac93255e81c81191671
seI3en <59121740+seI3en@users.noreply.github.com>
2021-06-12 12:45:55 +0200

    Revert "Update server"
    This reverts commit aef16617b5cadaa4babeed04365747cf0e2b866e.

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ function createRfbConnection(config, socket) {
       password: config.password,
       securityType: 'vnc',
     });
-    r.requestRedraw();
   } catch (e) {
     console.log(e);
   }
@@ -52,6 +51,7 @@ function addEventHandlers(r, socket) {
   });
 
   r.on('raw', function (rect) {
+    console.log('42424242');
     !initialized && handleConnection(rect.width, rect.height);
     socket.emit('frame', {
       x: rect.x,
